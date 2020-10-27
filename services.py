c1270d9eef85a8a64865f76ef4498ae5f8aaa80d
Tim Maycock <maycocktim@gmail.com>
2020-10-27 14:41:54 -0700

    First Commit

---
+++ services.py
@@ -0,0 +1,19 @@
+import tweepy
+
+
+# Tweepy Auth function
+def tweepy_auth(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET):
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+
+# Tweepy retweet likes function
+def retweet_favorites(api, username):
+    for tweet in api.favorites(username):
+        try:
+            api.retweet(tweet.id)
+        except tweepy.TweepError as e:
+            print(e)
+        return print("Done")
